c7633e66bffedc0202476e0e6a72f346d5d8d0f6
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2025-12-17 22:19:04 +0530

    [Raghav sharma] : added KilometersToMilesConversion program

---
+++ gcr-codebase/programming-elements/Level-1/KilometersToMilesConversion.java
@@ -0,0 +1,14 @@
+//Creating Class with name KilometersToMilesConversion indicating the purpose is to convert kilometers into miles.
+class KilometersToMilesConversion{
+    public static void main(String args[]){
+         // Declaring variable to store distance in kilometers
+         double kilometers = 10.8;
+
+         // Converting kilometers to miles.
+         double miles = kilometers * 1.6;
+
+         // Displaying converted distance.
+         System.out.println("The distance " + kilometers +  " km in miles is " + miles);
+    }
+}
+
