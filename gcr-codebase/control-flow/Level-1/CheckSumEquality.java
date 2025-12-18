b9ec046d8d08efac0b2864ea405c45340fa34657
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2025-12-18 21:42:42 +0530

    [Raghav sharma] : added CheckSumEquality program

---
+++ gcr-codebase/control-flow/Level-1/CheckSumEquality.java
@@ -0,0 +1,26 @@
+import java.util.*;
+// Creating a class with name CheckSumEquality to check Sum equality
+public class CheckSumEquality{
+   public static void main(String args[]){
+       // Creating scanner object to take user input of number variable
+       Scanner input = new Scanner(System.in);
+       int number = input.nextInt();
+       int sum = 0;
+       int i = 1;
+
+       // Checking whether number is natural number or not and compute sum.
+       if(number > 0){
+          int sumByFormula = number * (number + 1) / 2;
+          while(number >= i){
+             sum += i;
+             i++;
+          }
+          System.out.println("Sum using while loop : " + sum);
+          System.out.println("Sum using formula : " + sumByFormula);
+          System.out.println("Both results are equal : " + (sum == sumByFormula));
+       }
+       else{
+         System.out.println("Number is not natural number");
+       }
+   }
+}
