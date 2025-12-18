8443bc870930b291468d8051f98bd4504de7fc4d
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2025-12-18 19:42:35 +0530

    [Raghav sharma] : added CheckDivisibility program

--- gcr-codebase/control-flow/Level-1/CheckDivisibility.java
+++ gcr-codebase/control-flow/Level-1/CheckDivisibility.java
@@ -4,7 +4,6 @@ class CheckDivisibility{
     public static void main(String args[]){
        // Creating scanner object to take number as user input
        Scanner input = new Scanner(System.in);
-       System.out.print("Enter the number : ");
        int number = input.nextInt();
 
        // Check divisibility by 5 using modulus operator
