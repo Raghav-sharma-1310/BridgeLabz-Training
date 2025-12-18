8d587a8f96998e5ad23bcc10b4230582692f8d28
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2025-12-18 19:22:15 +0530

    [Raghav sharma] : added CheckDivisibility program

---
+++ gcr-codebase/control-flow/Level-1/CheckDivisibility.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+//Creating a class with name CheckDivisibility to indicating the purpose is to check if a number is divisible by 5 or not.
+class CheckDivisibility{
+    public static void main(String args[]){
+       // Creating scanner object to take number as user input
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the number : ");
+       int number = input.nextInt();
+
+       // Check divisibility by 5 using modulus operator
+       boolean isdivisible = (number % 5 == 0);
+
+       // Display the result
+       System.out.println("Is the number " + number + " divisible by 5? " + isdivisible);
+    }
+}
