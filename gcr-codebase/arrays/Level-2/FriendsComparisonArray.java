85864aa02446a23c2caec45eaab6e4887717da0f
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2025-12-20 11:01:01 +0530

    [Raghav sharma] : added FriendsComparisonArray program

---
+++ gcr-codebase/arrays/Level-2/FriendsComparisonArray.java
@@ -0,0 +1,40 @@
+import java.util.*;
+// Create a class with name FriendsComparisonArray to find the youngest and tallest among three friends
+public class FriendsComparisonArray {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] age = new int[3];
+        double[] height = new double[3];
+
+        // Storing age and height
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter age of " + names[i] + ": ");
+            age[i] = input.nextInt();
+
+            System.out.print("Enter height of " + names[i] + ": ");
+            height[i] = input.nextDouble();
+        }
+
+        int youngestIndex = 0;
+        int tallestIndex = 0;
+
+        // Finding youngest and tallest
+        for (int i = 1; i < 3; i++) {
+            if (age[i] < age[youngestIndex]) {
+                youngestIndex = i;
+            }
+            if (height[i] > height[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        // Displaying results
+        System.out.println("\nYoungest Friend: " + names[youngestIndex]);
+        System.out.println("Tallest Friend: " + names[tallestIndex]);
+
+        input.close();
+    }
+}
