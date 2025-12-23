051476cd4946bd7bd480a102bd474a1305951477
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2025-12-23 12:06:30 +0530

    [Raghav sharma] : added CharacterTypeDisplay Program

---
+++ gcr-codebase/strings/Level-2/CharacterTypeDisplay.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class CharacterTypeDisplay {
+
+    public static String checkCharacter(char ch) {
+        if (ch >= 'A' && ch <= 'Z') {
+            ch = (char) (ch + 32);
+        }
+        if (ch < 'a' || ch > 'z') return "Not a Letter";
+        if ("aeiou".indexOf(ch) >= 0) return "Vowel";
+        return "Consonant";
+    }
+
+    public static String[][] analyzeCharacters(String text) {
+        String[][] result = new String[text.length()][2];
+
+        for (int i = 0; i < text.length(); i++) {
+            result[i][0] = String.valueOf(text.charAt(i));
+            result[i][1] = checkCharacter(text.charAt(i));
+        }
+        return result;
+    }
+
+    public static void displayTable(String[][] data) {
+        System.out.println("\nCHARACTER\tTYPE");
+        for (String[] row : data) {
+            System.out.println(row[0] + "\t\t" + row[1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        String[][] result = analyzeCharacters(text);
+        displayTable(result);
+    }
+}
