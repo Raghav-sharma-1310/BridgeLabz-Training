04a9a428833145d7f0cfa01c3280b01d697685a1
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2025-12-23 21:26:26 +0530

    [Raghav sharma] : added VotingEligibility Program

--- gcr-codebase/strings/Level-2/VotingEligibility.java
+++ gcr-codebase/strings/Level-2/VotingEligibility.java
@@ -2,6 +2,7 @@ import java.util.Random;
 
 public class VotingEligibility {
 
+    // Method to find random ages
     public static int[] generateAges(int n) {
         Random r = new Random();
         int[] ages = new int[n];
@@ -11,6 +12,7 @@ public class VotingEligibility {
         return ages;
     }
 
+    // Check voting eligibility
     public static String[][] checkVotingEligibility(int[] ages) {
         String[][] result = new String[ages.length][2];
         for (int i = 0; i < ages.length; i++) {
@@ -20,6 +22,7 @@ public class VotingEligibility {
         return result;
     }
 
+    // Method to display this in tabular format
     public static void displayTable(String[][] data) {
         System.out.println("AGE\tCAN VOTE");
         for (String[] row : data) {
