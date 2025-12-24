d3aedc0992e04c94fd70f8fe3f6842b480f45c87
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2025-12-24 09:21:16 +0530

    [Raghav sharma] : added BasicCalculator Program

---
+++ gcr-codebase/extras/extra-built-in/Level-2/BasicCalculator.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("Choose operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Result: " + add(num1, num2));
+                break;
+            case 2:
+                System.out.println("Result: " + subtract(num1, num2));
+                break;
+            case 3:
+                System.out.println("Result: " + multiply(num1, num2));
+                break;
+            case 4:
+                System.out.println("Result: " + divide(num1, num2));
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+
+        sc.close();
+    }
+}
