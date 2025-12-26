9d474d9bea2bf28a6591e75c0e166fb39f9531e9
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2025-12-26 22:20:34 +0530

    [Raghav sharma] : added TrappingRainWater program

---
+++ leet-code-codebase/arrays/TrappingRainWater0042.java
@@ -0,0 +1,50 @@
+// Program to find the trapped rain water 
+import java.util.Scanner;
+
+public class TrappingRainWater0042 {
+
+    public int trap(int[] height) {
+        int n = height.length;
+
+        int leftmax[] = new int[n];
+        leftmax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftmax[i] = Math.max(height[i], leftmax[i - 1]);
+        }
+
+        int rightmax[] = new int[n];
+        rightmax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightmax[i] = Math.max(height[i], rightmax[i + 1]);
+        }
+
+        int trappedwater = 0;
+        for (int i = 0; i < n; i++) {
+            int waterlevel = Math.min(leftmax[i], rightmax[i]);
+            trappedwater += waterlevel - height[i];
+        }
+
+        return trappedwater;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of bars: ");
+        int n = sc.nextInt();
+
+        int[] height = new int[n];
+
+        System.out.println("Enter height of bars:");
+        for (int i = 0; i < n; i++) {
+            height[i] = sc.nextInt();
+        }
+
+        TrappingRainWater0042 obj = new TrappingRainWater0042();
+        int result = obj.trap(height);
+
+        System.out.println("Total Trapped Water: " + result);
+
+        sc.close();
+    }
+}
