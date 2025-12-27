17f64571f45be6b8e00b17140fe970666c6ce7fe
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2025-12-28 00:00:11 +0530

    [Raghav sharma] : added BestTimeToBuyAndSellStock Program

---
+++ leet-code-codebase/arrays/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,24 @@
+public class BestTimeToBuyAndSellStock {
+
+    public static int maxProfit(int[] prices) {
+        int buyPrice = Integer.MAX_VALUE; // +INFINITY
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        int result = maxProfit(prices);
+        System.out.println("Maximum Profit: " + result);
+    }
+}
