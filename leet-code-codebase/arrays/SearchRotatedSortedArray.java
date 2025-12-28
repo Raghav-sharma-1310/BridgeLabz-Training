a53e8d3341af290e3b481b4fd83cc2e60544ef69
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2025-12-28 22:42:12 +0530

    [Raghav sharma] : added SearchRotatedSortedArray Program

---
+++ leet-code-codebase/arrays/SearchRotatedSortedArray.java
@@ -0,0 +1,51 @@
+public class SearchRotatedSortedArray {
+
+    // Method to search target in a rotated sorted array
+    public int search(int[] nums, int target) {
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[start] <= nums[mid]) {
+
+                if (nums[start] <= target && target < nums[mid]) {
+                    end = mid - 1;
+                } 
+                else {
+                    start = mid + 1;
+                }
+            } 
+            else {
+
+                if (nums[mid] < target && target <= nums[end]) {
+                    start = mid + 1;
+                } 
+                else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        SearchRotatedSortedArray obj = new SearchRotatedSortedArray();
+
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target1 = 0;
+        int target2 = 3;
+
+        System.out.println("Index of " + target1 + ": " + obj.search(nums, target1));
+        System.out.println("Index of " + target2 + ": " + obj.search(nums, target2));
+    }
+}
