8b70f14eae72f28c69fac3874f08e9bf26a0289c
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2025-12-31 19:29:00 +0530

    [Raghav sharma] : added SmartHomeSystem class Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/singleinheritance/smarthomedevices/SmartHomeSystem.java
@@ -0,0 +1,10 @@
+package com.inheritance.singleinheritance.smarthomedevices;
+
+public class SmartHomeSystem {
+    public static void main(String[] args) {
+
+        Thermostat thermostat = new Thermostat( "TH-101","ON", 24);
+
+        thermostat.displayStatus();
+    }
+}
