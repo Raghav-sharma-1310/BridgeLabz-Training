22db955aeac17cabd25ac3220b2ded055c2fc017
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2025-12-31 20:10:47 +0530

    [Raghav sharma] : added Online Course Class Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelinheritance/educationalcourse/OnlineCourse.java
@@ -0,0 +1,12 @@
+package com.inheritance.multilevelinheritance.educationalcourse;
+
+public class OnlineCourse extends Course {
+    String platform;
+    boolean isRecorded;
+
+    OnlineCourse(String courseName, int duration, String platform, boolean isRecorded) {
+        super(courseName, duration);
+        this.platform = platform;
+        this.isRecorded = isRecorded;
+    }
+}
