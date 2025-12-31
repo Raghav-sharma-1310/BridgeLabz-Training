964e6d8d87ead065833bcdfee4b2c33851d595aa
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2025-12-31 20:08:49 +0530

    [Raghav sharma] : added Course Class Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelinheritance/educationalcourse/Course.java
@@ -0,0 +1,11 @@
+package com.inheritance.multilevelinheritance.educationalcourse;
+
+public class Course {
+    String courseName;
+    int duration; 
+
+    Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+}
