57f518838156d49bc0b5912edc346eb38b095187
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2026-01-01 10:29:20 +0530

    [Raghav sharma] : added Cat Class Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hiererchicalinheritance/animalhierarchy/Cat.java
@@ -0,0 +1,13 @@
+package com.inheritance.hiererchicalinheritance.animalhierarchy;
+
+public class Cat extends Animal {
+
+    Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println("Cat meows");
+    }
+}
