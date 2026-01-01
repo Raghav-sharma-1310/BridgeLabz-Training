7f4d054e0d268c0c8759278dae9ab48f0f4eb4e6
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2026-01-01 10:33:37 +0530

    [Raghav sharma] : added Staff Class Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hiererchicalinheritance/schoolsystem/Staff.java
@@ -0,0 +1,14 @@
+package com.inheritance.hiererchicalinheritance.schoolsystem;
+
+public class Staff extends Person {
+    String department;
+
+    Staff(String name, int age, String department) {
+        super(name, age);
+        this.department = department;
+    }
+
+    void displayRole() {
+        System.out.println("Role: Staff");
+    }
+}
