9a8608c0bde310353b3f55be8ada2749ba73f2c2
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2026-01-01 10:34:07 +0530

    [Raghav sharma] : added Student Class Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hiererchicalinheritance/schoolsystem/Student.java
@@ -0,0 +1,14 @@
+package com.inheritance.hiererchicalinheritance.schoolsystem;
+
+public class Student extends Person {
+    String grade;
+
+    Student(String name, int age, String grade) {
+        super(name, age);
+        this.grade = grade;
+    }
+
+    void displayRole() {
+        System.out.println("Role: Student");
+    }
+}
