6b3ed0cb95363f3652f11a56f80014cfcff4d610
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2026-01-01 10:38:37 +0530

    [Raghav sharma] : added Checking Account Class Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hiererchicalinheritance/bankaccounttype/CheckingAccount.java
@@ -0,0 +1,14 @@
+package com.inheritance.hiererchicalinheritance.bankaccounttype;
+
+public class CheckingAccount extends BankAccount {
+    double withdrawalLimit;
+
+    CheckingAccount(String accountNumber, double balance, double withdrawalLimit) {
+        super(accountNumber, balance);
+        this.withdrawalLimit = withdrawalLimit;
+    }
+
+    void displayAccountType() {
+        System.out.println("Account Type: Checking Account");
+    }
+}
