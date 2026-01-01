f6491b2d3702e69b9a5659e29cf70ad1cc03175f
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2026-01-01 10:36:22 +0530

    [Raghav sharma] : added Bank Account Class Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hiererchicalinheritance/bankaccounttype/BankAccount.java
@@ -0,0 +1,11 @@
+package com.inheritance.hiererchicalinheritance.bankaccounttype;
+
+public class BankAccount {
+	String accountNumber;
+    double balance;
+
+    BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+}
