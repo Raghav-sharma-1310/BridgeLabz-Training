afe306b804e9b36926cb3174384fbdd5aea8ea00
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2026-01-01 10:12:04 +0530

    [Raghav sharma] : added Petrol Vehicle Class Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/vehiclemanagement/PetrolVehicle.java
@@ -0,0 +1,12 @@
+package com.inheritance.hybridinheritance.vehiclemanagement;
+
+public class PetrolVehicle extends Vehicle implements Refuelable {
+
+    PetrolVehicle(String model, int maxSpeed) {
+        super(model, maxSpeed);
+    }
+
+    public void refuel() {
+        System.out.println("Petrol vehicle is refueling");
+    }
+}
