b2cf9005858c0d511a1c84a4bc8d5a0f4b9f67a5
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2026-01-01 10:24:46 +0530

    [Raghav sharma] : added Vehicle Class Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/vehiclemanagement/Vehicle.java
@@ -0,0 +1,11 @@
+package com.inheritance.hybridinheritance.vehiclemanagement;
+
+public class Vehicle {
+    String model;
+    int maxSpeed;
+
+    Vehicle(String model, int maxSpeed) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+    }
+}
