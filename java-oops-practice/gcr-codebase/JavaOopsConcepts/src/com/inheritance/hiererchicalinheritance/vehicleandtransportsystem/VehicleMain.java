7fd90da8ae7abde4b09cb2dfc483425cb671f004
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2026-01-01 10:55:10 +0530

    [Raghav sharma] : added Vehicle Main Class Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hiererchicalinheritance/vehicleandtransportsystem/VehicleMain.java
@@ -0,0 +1,16 @@
+package com.inheritance.hiererchicalinheritance.vehicleandtransportsystem;
+
+public class VehicleMain {
+    public static void main(String[] args) {
+
+        Vehicle[] vehicles = new Vehicle[3];
+        vehicles[0] = new Car(180, "Petrol", 5);
+        vehicles[1] = new Truck(120, "Diesel", 10);
+        vehicles[2] = new Motorcycle(150, "Petrol");
+
+        for (Vehicle v : vehicles) {
+            v.displayInfo();
+            System.out.println();
+        }
+    }
+}
