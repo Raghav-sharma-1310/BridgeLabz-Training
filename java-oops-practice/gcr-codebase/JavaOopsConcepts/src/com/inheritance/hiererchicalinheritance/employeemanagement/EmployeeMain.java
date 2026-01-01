887136c6319f2801687c2785b675d0c8579ef75c
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2026-01-01 10:46:36 +0530

    [Raghav sharma] : added Employee Main Class Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hiererchicalinheritance/employeemanagement/EmployeeMain.java
@@ -0,0 +1,18 @@
+package com.inheritance.hiererchicalinheritance.employeemanagement;
+
+public class EmployeeMain {
+    public static void main(String[] args) {
+
+        Employee e1 = new Manager("Ram", 101, 80000, 5);
+        Employee e2 = new Developer("Aman", 102, 60000, "Java");
+        Employee e3 = new Intern("Neha", 103, 15000, 6);
+
+        e1.displayDetails();
+        System.out.println();
+
+        e2.displayDetails();
+        System.out.println();
+
+        e3.displayDetails();
+    }
+}
