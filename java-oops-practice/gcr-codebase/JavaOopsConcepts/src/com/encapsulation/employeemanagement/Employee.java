6301f1c33cd95d5756d14eb81a707257457ad84f
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2026-01-02 10:48:48 +0530

    [Raghav sharma] : added Employee Class Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/employeemanagement/Employee.java
@@ -0,0 +1,53 @@
+package com.encapsulation.employeemanagement;
+
+public abstract class Employee implements Department {
+
+    private int employeeId;
+    private String name;
+    private double baseSalary;
+    private String department;
+
+    // Constructor
+    public Employee(int employeeId, String name, double baseSalary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    public abstract double calculateSalary();
+     
+    public void displayDetails() {
+        System.out.println("Employee ID : " + employeeId);
+        System.out.println("Name        : " + name);
+        System.out.println("Department  : " + department);
+        System.out.println("Salary      : ₹" + calculateSalary());
+    }
+
+    // Interface methods
+    @Override
+    public void assignDepartment(String deptName) {
+        this.department = deptName;
+    }
+
+    @Override
+    public String getDepartmentDetails() {
+        return department;
+    }
+
+    // Getters and Setters 
+    public int getEmployeeId() {
+        return employeeId;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+}
