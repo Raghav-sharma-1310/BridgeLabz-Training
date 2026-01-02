66e2ff50a7bf71e49807d686e366bba12ac6827d
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2026-01-02 10:20:25 +0530

    [Raghav sharma] : added person Class Program

---
+++ java-oops-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/bankmanagementsystem/Person.java
@@ -0,0 +1,11 @@
+package com.bankmanagementsystem;
+
+public class Person {
+	protected String name ;
+	protected int id;
+	
+	Person(int id, String name){
+		this.id = id;
+		this.name = name;
+	}
+}
