f57ee37f8b57701c182164425306d529f502abe4
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2026-01-26 22:13:42 +0530

    [Raghav Sharma] : Added ResumeAnalyzer Program

---
+++ java-collections-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/day1/resumeanalyzer/InvalidFormatException.java
@@ -0,0 +1,9 @@
+package com.day1.resumeanalyzer;
+
+public class InvalidFormatException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public InvalidFormatException(String msg) {
+		super(msg);
+	}
+}
