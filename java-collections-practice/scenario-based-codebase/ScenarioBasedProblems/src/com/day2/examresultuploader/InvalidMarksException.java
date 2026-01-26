022b9a8177b331b17e7473f45edd9d97b509f406
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2026-01-26 22:19:54 +0530

    [Raghav Sharma] : Added Exam Result Uploader Program

---
+++ java-collections-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/day2/examresultuploader/InvalidMarksException.java
@@ -0,0 +1,9 @@
+package com.day2.examresultuploader;
+
+public class InvalidMarksException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+}
