ff9118888c5b46325c83168d422deeac216c27de
Raghav-sharma-1310 <sharmaraghav1854@gmail.com>
2026-01-26 22:20:25 +0530

    [Raghav Sharma] : Added MedInventory Program

---
+++ java-collections-practice/scenario-based-codebase/ScenarioBasedProblems/src/com/day2/medinventory/LowStockException.java
@@ -0,0 +1,9 @@
+package com.day2.medinventory;
+
+public class LowStockException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public LowStockException(String message) {
+		super(message);
+	}
+}
