77584dcd14a771bc55df8152a2bb25d488a6a731
Raghav Sharma <143438523+Raghav-sharma-1310@users.noreply.github.com>
2026-01-27 11:21:42 +0530

    [Raghav sharma] : Add space replacement logic in ReplaceSpaces class
    Implemented functionality to replace multiple spaces with a single space in the main method.

--- java-collections-practice/gcr-codebase/JavaCollectionsProblem/src/com/regex/ReplaceSpaces.java
+++ java-collections-practice/gcr-codebase/JavaCollectionsProblem/src/com/regex/ReplaceSpaces.java
@@ -1,10 +1,11 @@
 package com.regex;
 
 public class ReplaceSpaces {
-
-	public static void main(String[] args) {
-
-		
-	}
-
+    public static void main(String[] args) {
+        String input = "This   is   an    example   with   multiple     spaces.";
+        
+        String output = input.replaceAll("\\s+", " ");
+        
+        System.out.println(output);
+    }
 }
